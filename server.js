185ecc7e0d68c3553dd6f94b31a9adb9a84784a1
Cameron Roman <camroman95@gmail.com>
2019-04-07 19:30:53 -0500

    made change to server.js file

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path');
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public'));
+    res.sendFile(path.resolve(__dirname, 'public', 'img'));
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
