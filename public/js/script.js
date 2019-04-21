22b5fa53faf531e67b1ab15cf9cce83be25e8071
Cameron Roman <camroman95@gmail.com>
2019-04-20 20:11:25 -0500

    Changed fixed nav class in script.js

--- public/js/script.js
+++ public/js/script.js
@@ -60,14 +60,6 @@ $(document).ready(function() {
 
         var body = $("body");
 
-        if($(window).scrollTop() >= navTop) {
-            body.css("padding-top", nav.outerHeight() + "px");
-            body.addClass("fixedNav");
-        }
-        else {
-            body.css("padding-top", 0);
-            body.removeClass("fixedNav");
-        }
     }
 
 
