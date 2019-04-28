fb9bfa7182a3a82092929edd62b5667dff9da72c
Cameron Roman <camroman95@gmail.com>
2019-04-28 11:28:45 -0500

    hopefully fixed http request.

--- server.js
+++ server.js
@@ -10,5 +10,13 @@ app.use(express.static('public'));
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public'));
 });
+if(process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+      if (req.header('x-forwarded-proto') !== 'https')
+        res.redirect(`https://${req.header('host')}${req.url}`)
+      else
+        next()
+    })
+  }
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
