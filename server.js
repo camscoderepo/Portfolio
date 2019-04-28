80f6288909d4bae8f2fbdc9c2d29efefdd83a524
Cameron Roman <camroman95@gmail.com>
2019-04-28 11:32:32 -0500

    Added ssl redirect module.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const sslRedirect = require('heroku-ssl-redirect');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
@@ -7,16 +7,11 @@ const path = require('path');
 
 app.use(express.static('public'));
 
+app.use(sslRedirect());
+
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public'));
 });
-if(process.env.NODE_ENV === 'production') {
-    app.use((req, res, next) => {
-      if (req.header('x-forwarded-proto') !== 'https')
-        res.redirect(`https://${req.header('host')}${req.url}`)
-      else
-        next()
-    })
-  }
+
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
