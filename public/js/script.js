7d4816166a7f64b5c925f2ee1e736d8e2dec0f1b
Cameron Roman <camroman95@gmail.com>
2019-05-29 14:35:59 -0500

    Made navbar stick to screen smoothly.

--- public/js/script.js
+++ public/js/script.js
@@ -57,8 +57,16 @@ $(document).ready(function() {
     $(window).on("scroll", stickyNavigation);
 
     function stickyNavigation() {
-
         var body = $("body");
+
+        if($(window).scrollTop() >= navTop) {
+            body.css("padding-top", nav.outerHeight() + "px");
+            body.addClass("fixedNav");
+        }
+        else {
+            body.css("padding-top", 0);
+            body.removeClass("fixedNav");
+        }
 
     }
 
