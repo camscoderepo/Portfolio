dce73d08d1abe21aa21da2ca85f4a8d931cb74ad
Cameron Roman <camroman95@gmail.com>
2020-03-05 18:03:28 -0600

    changed ui, making push to live site

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,38 +1,48 @@
-const repos = document.querySelector('.repos');
+
 
 
 
 //Promise to get github repos
-const getRepoNames = async () =>
-{
-        const response = await fetch(`https://api.github.com/users/camscoderepo/repos`);
-        if(!response.status){
-            throw new Error('Network response was not good')
-        }
-        const data = await response.json()
+// const getRepoNames = async () =>
+// {
+//     const response = await fetch(`https://api.github.com/users/camscoderepo/repos`);
+//     if(!response.status){
+//         throw new Error('Network response was not good');
+//     }
+//     const data = await response.json()
 
-        const names = await data.map(name => {
-            return name.name;
-        }) 
-        return names
-    } 
+//     const names = await data.map(name => {
+//         return name.name;
+//     }) 
+//     return names;    
+// } 
 
-function getRepoNames(repos) {
-    const res = fetch(`https://api.github.com/users/camscoderepo/repos`)
+const getRepoNames = () => {
+    const response = fetch(`https://api.github.com/users/camscoderepo/repo`);
+    if(!response.status){
+        throw new Error('Network response was not good');
+    }
+    const data = response.json()
+
+    const names = data.map(name => {
+        return name.name
+    })
+    const repos = document.querySelector('.repos');
+    repos.innerHTML += names;
 }
 
 
 
-//render recipe data
+// //render recipe data
 
-const renderRepo = (repos) => {
-    const html = `
-    <li onclick="" class="">
-      <div class="imageOverlay"></div>
-    </div>
-   </li>
-    `;
-    repos.innerHTML += html
-};
+// const renderRepo = (repos) => {
+//     const html = `
+//     <li onclick="" class="">
+//       <div class="imageOverlay"></div>
+//     </div>
+//    </li>
+//     `;
+//     repos.innerHTML += html
+// };
 
-document.addEventListener()
+// document.addEventListener()
